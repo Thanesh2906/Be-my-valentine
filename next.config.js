4d3e2fda9dfb4ea6a160e673131922f3988b3095
Thaneshvaran Subramaniam <107298547+Thanesh2906@users.noreply.github.com>
2026-02-13 20:59:07 +0800

    Revert "Update next.config.js"
    This reverts commit b5ea935a36627de43336b610f8b7d9a58a7b9915.

--- next.config.js
+++ next.config.js
@@ -5,7 +5,10 @@ const nextConfig = {
   images: {
     // Required when using `output: 'export'` with the App Router
     unoptimized: true
-  }
+  },
+  // If you deploy to GitHub Pages under /Be-my-valentine, uncomment these:
+  basePath: '/Be-my-valentine',
+  assetPrefix: '/Be-my-valentine/'
 };
 
 module.exports = nextConfig;
